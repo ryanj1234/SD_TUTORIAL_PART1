b3b8da655d91629df91ee0a933c1da6aefa291a9
ryanj1234 <44788746+ryanj1234@users.noreply.github.com>
2018-11-05 16:04:25 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,140 @@
+#include<avr/io.h>
+#include<util/delay.h>
+
+// pin definitions
+#define DDR_SPI         DDRB
+#define PORT_SPI        PORTB
+#define CS              PINB2
+#define MOSI            PINB3
+#define MISO            PINB4
+#define SCK             PINB5
+
+// macros
+#define CS_ENABLE()     PORT_SPI  &= ~(1 << CS)
+#define CS_DISABLE()    PORT_SPI |= (1 << CS)
+
+// command definitions
+#define CMD0        0
+#define CMD0_ARG    0x00000000
+#define CMD0_CRC    0x94
+
+// SPI functions
+void SPI_init(void);
+uint8_t SPI_transfer(uint8_t data);
+
+// SD functions
+void SD_powerUpSeq(void);
+void SD_command(uint8_t cmd, uint32_t arg, uint8_t crc);
+uint8_t SD_readRes1(void);
+uint8_t SD_goIdleState(void);
+
+int main(void)
+{
+    // initialize SPI
+    SPI_init();
+
+    // start power up sequence
+    SD_powerUpSeq();
+
+    // command card to idle
+    SD_goIdleState();
+
+    while(1);
+}
+
+void SPI_init()
+{
+    // set CS, MOSI and SCK to output
+    DDR_SPI |= (1 << CS) | (1 << MOSI) | (1 << SCK);
+
+    // enable pull up resistor in MISO
+    DDR_SPI |= (1 << MISO);
+
+    // enable SPI, set as master, and clock to fosc/128
+	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0);
+}
+
+uint8_t SPI_transfer(uint8_t data)
+{
+    // load data into register
+    SPDR = data;
+
+    // Wait for transmission complete
+    while(!(SPSR & (1 << SPIF)));
+
+    // return SPDR
+    return SPDR;
+}
+
+void SD_powerUpSeq()
+{
+    // make sure card is deselected
+    CS_DISABLE();
+
+    // give SD card time to power up
+    _delay_ms(1);
+
+    // select SD card
+    SPI_transfer(0xFF);
+    CS_ENABLE();
+
+    // send 74 clock cycles to synchronize
+    for(uint8_t i = 0; i < 74; i++)
+        SPI_transfer(0xFF);
+
+    // deselect SD card
+    CS_DISABLE();
+    SPI_transfer(0xFF);
+}
+
+void SD_command(uint8_t cmd, uint32_t arg, uint8_t crc)
+{
+    // transmit command to sd card
+    SPI_transfer(cmd|0x40);
+
+    // transmit argument
+    SPI_transfer((uint8_t)(arg >> 24));
+    SPI_transfer((uint8_t)(arg >> 16));
+    SPI_transfer((uint8_t)(arg >> 8));
+    SPI_transfer((uint8_t)(arg));
+
+    // transmit crc
+    SPI_transfer(crc|0x01);
+}
+
+uint8_t SD_readRes1()
+{
+    uint8_t i = 0, res1;
+
+    // keep polling until actual data received
+    while((res1 = SPI_transfer(0xFF)) == 0xFF)
+    {
+        i++;
+
+        // if no data received for 8 bytes, break
+        if(i > 8) break;
+    }
+
+    return res1;
+}
+
+uint8_t SD_goIdleState()
+{
+    // assert chip select
+    SPI_transfer(0xFF);
+    CS_ENABLE();
+    SPI_transfer(0xFF);
+
+    // send CMD0
+    SD_command(CMD0, CMD0_ARG, CMD0_CRC);
+
+    // read response
+    uint8_t res1 = SD_readRes1();
+
+    // deassert chip select
+    SPI_transfer(0xFF);
+    CS_DISABLE();
+    SPI_transfer(0xFF);
+
+    return res1;
+}
